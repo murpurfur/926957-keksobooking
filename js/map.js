540a6f328d516423dbc7da6cef19bd265b247734
Ari <murpurfur@gmail.com>
2018-11-29 00:05:06 +0300

    8 random object creation in map.js

--- js/map.js
+++ js/map.js
@@ -1,6 +1,41 @@
 var randomValue = function(start, end) {
-  Math.floor(Math.random() * end + start)
+  return Math.floor(Math.random() * end + start)
 }
+
+var test = randomValue(1, 4)
+
+var typesList = ['palace', 'flat', 'house', 'bungalo']
+
+var checkInOutTime = ['12:00', '13:00', '14:00']
+
+var featuresList = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner']
+var featuresListRandom = randomValue(1, featuresList.length)
+
+var photosList = [
+  'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel3.jpg'
+]
+
+var shuffleArray = function(array) {
+  var currentIndex = array.length,
+    temporaryValue,
+    randomIndex
+  while (0 !== currentIndex) {
+    randomIndex = Math.floor(Math.random() * currentIndex)
+    currentIndex -= 1
+    temporaryValue = array[currentIndex]
+    array[currentIndex] = array[randomIndex]
+    array[randomIndex] = temporaryValue
+  }
+  return array
+}
+
+var author = {
+  avatar: 'img/avatars/user' + '0' + randomValue(1, 8) + '.png'
+}
+
+var map = document.querySelector('.map')
 
 var titlesList = [
   'Большая уютная квартира',
@@ -13,62 +48,25 @@ var titlesList = [
   'Неуютное бунгало по колено в воде'
 ]
 
-/*
-функция которая возвращает рандомную строку из массива 
+var objectArray = []
 
-выбрать рандомный элемент массива
-вывести в строку
-удалить из массива
-повторить 
-  пока количество элементов больше 0
-*/
+for (var i = 0; i < 8; i++) {
+  var randomStringNumber = randomValue(0, titlesList.length)
 
-var typesList = ['palace', 'flat', 'house', 'bungalo']
-
-var checkInOutTime = ['12:00', '13:00', '14:00']
-
-var featuresList = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner']
-
-// var generatePhotosList = function() {
-//   var photosList = [];
-//   for (var i = 0; i < 3; i++) {
-//     photosList[i] = 'http://o0.github.io/assets/images/tokyo/hotel' + (i + 1) + '.jpg'
-//   }
-// }
-
-var photosList = [
-  'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
-  'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
-  'http://o0.github.io/assets/images/tokyo/hotel3.jpg'
-]
-
-var author = {
-  avatar: 'img/avatars/user' + '0' + randomValue(1, 8) + '.png'
+  objectArray.push({
+    title: titlesList[randomStringNumber],
+    address: randomValue(1, map.scrollWidth) + ', ' + randomValue(130, 630),
+    price: randomValue(1000, 1000000),
+    type: typesList[randomValue(0, typesList.length - 1)],
+    rooms: randomValue(1, 5),
+    guests: randomValue(1, 8),
+    checkin: checkInOutTime[randomValue(0, 3)],
+    checkout: checkInOutTime[randomValue(0, 3)],
+    features: featuresList.splice(0, featuresListRandom),
+    description: '',
+    photos: shuffleArray(photosList)
+  })
+  titlesList.splice(randomStringNumber, 1)
 }
 
-var offer = {
-  title: 'строка, заголовок предложения, одно из фиксированных значений "Большая уютная квартира"',
-  // "Маленькая неуютная квартира", "Огромный прекрасный дворец", "Маленький ужасный дворец", "Красивый гостевой домик",
-  // "Некрасивый негостеприимный домик", "Уютное бунгало далеко от моря", "Неуютное бунгало по колено в воде".
-  // Значения не должны повторяться.
-  address: 'строка, адрес предложения, представляет собой запись вида "{{location.x}}, {{location.y}}"',
-  //например, "600, 350"
-  price: randomValue(1000, 1000000),
-  type: typesList[randomValue(0, typesList.length - 1)],
-  rooms: randomValue(1, 5),
-  guests: randomValue(1, 8),
-  checkin: checkInOutTime[randomValue(0, 3)],
-  checkout: checkInOutTime[randomValue(0, 3)],
-  features: featuresList.splice(0, randomValue(0, featuresList.lenght)),
-  description: '',
-  photos: []
-  //массив из строк "http://o0.github.io/assets/images/tokyo/hotel1.jpg", "http://o0.github.io/assets/images/tokyo/hotel2.jpg"
-  //и "http://o0.github.io/assets/images/tokyo/hotel3.jpg" расположенных в произвольном порядке
-}
-
-var location = {
-  x: '',
-  //случайное число, координата x метки на карте. Значение ограничено размерами блока, в котором перетаскивается метка.
-  y: randomValue(130, 630)
-  //случайное число, координата y метки на карте от 130 до 630.
-}
+console.log(objectArray)
