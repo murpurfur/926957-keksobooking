2a5bbf97049b1c9045c40791a9c2b7d9eb0fb3e8
Ari <murpurfur@gmail.com>
2019-01-28 00:30:08 +0300

    limits for uploaded images

--- js/images.js
+++ js/images.js
@@ -2,6 +2,7 @@
 
 (function () {
   var FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
+  var UPLOADED_IMAGES_LIMIT = 8;
 
   // ----- Добавление аватарки
   var avatarChooser = document.querySelector('.ad-form-header__input');
@@ -29,6 +30,7 @@
   var adImageChooser = document.querySelector('.ad-form__input');
   var adImagePreview = document.querySelector('.ad-form__photo');
   var imageContainer = document.querySelector('.ad-form__photo-container');
+  var imageCount = 0;
 
   adImageChooser.addEventListener('change', function () {
     var file = adImageChooser.files[0];
@@ -47,9 +49,16 @@
       });
 
       reader.readAsDataURL(file);
-      var dupImagePreview = adImagePreview.cloneNode(true);
-      imageContainer.insertBefore(dupImagePreview, adImagePreview);
+      imageCount = imageCount + 1;
 
+      if (imageCount < UPLOADED_IMAGES_LIMIT) {
+        var dupImagePreview = adImagePreview.cloneNode(true);
+        imageContainer.insertBefore(dupImagePreview, adImagePreview.nextSibling);
+      } else if (imageCount === UPLOADED_IMAGES_LIMIT) {
+        dupImagePreview = adImagePreview.cloneNode(true);
+        imageContainer.insertBefore(dupImagePreview, adImagePreview.nextSibling);
+        imageContainer.children[UPLOADED_IMAGES_LIMIT + 1].remove();
+      }
     }
   });
 })();
