0bb9b6d8e32a6d5c2a968ef6517eb4d354bf7636
Ari <murpurfur@gmail.com>
2019-01-28 18:50:35 +0300

    card removes (on success screen)

--- js/pageReset.js
+++ js/pageReset.js
@@ -1,13 +1,17 @@
 'use strict';
 (function () {
   var form = window.utils.notice.querySelector('.ad-form');
-
+  var closeCardHandler = function () {
+    var addedCard = window.utils.map.querySelector('.map__card');
+    addedCard.remove();
+  };
   // ----- Для очистки превью картинок
   var adImagePreview = document.querySelector('.ad-form__photo').children[0];
   var avararPreview = document.querySelector('.ad-form-header__preview').children[0];
   // ----- Функция перевода страницы в неактивное состояние
   var pageReset = function () {
     form.reset();
+    closeCardHandler();
     window.resetPriceField('flat');
     // ----- Очистка превью картинок
     avararPreview.src = 'img/muffin-grey.svg';
